302f4481e4cb9651415970d76a080b5a66ee4c28
Nick Fox-Gieg <nick@fox-gieg.com>
2020-07-30 22:04:42 -0400

    rpi shell script

---
+++ setup-rpi.sh
@@ -0,0 +1,18 @@
+# https://github.com/acrobotic/Ai_Demos_RPi/wiki/Raspberry-Pi-4-and-Intel-RealSense-D435
+
+sudo apt-get install automake libtool vim cmake libusb-1.0-0-dev libx11-dev xorg-dev libglu1-mesa-dev
+
+mkdir setup
+cd setup
+git clone https://github.com/IntelRealSense/librealsense
+cd librealsense
+sudo cp config/99-realsense-libusb.rules /etc/udev/rules.d/
+
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+
+mkdir build
+cd build
+cmake .. -DBUILD_EXAMPLES=true -DCMAKE_BUILD_TYPE=Release -DFORCE_LIBUVC=true
+make
+sudo make install
