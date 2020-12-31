b64c82aea15206d33438fbf45881cf02d88b201f
Nick Fox-Gieg <nick@fox-gieg.com>
2020-12-31 01:55:22 -0500

    shell

---
+++ getlib-linux64.sh
@@ -0,0 +1,8 @@
+URL=https://fox-gieg.com/patches/github/n1ckfg/ofxRealSense2/libs/librealsense2/lib/linux64
+
+sudo curl $URL/librealsense2.so --output /usr/lib/librealsense2.so
+
+sudo cp libs/librealsense2/lib/linux64/99-realsense-libusb.rules /etc/udev/rules.d/99-realsense-libusb.rules
+
+sudo udevadm control --reload-rules
+sudo udevadm trigger
