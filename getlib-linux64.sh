ec2105a70655f02f38227f5322db118c07d1b50d
Nick Fox-Gieg <nick@fox-gieg.com>
2020-12-31 02:06:09 -0500

    reorganize

--- getlib-linux64.sh
+++ getlib-linux64.sh
@@ -2,7 +2,7 @@ URL=https://fox-gieg.com/patches/github/n1ckfg/ofxRealSense2/libs/librealsense2/
 
 sudo curl $URL/librealsense2.so --output /usr/lib/librealsense2.so
 
-sudo cp libs/librealsense2/lib/linux64/99-realsense-libusb.rules /etc/udev/rules.d/99-realsense-libusb.rules
+sudo cp libs/librealsense2/lib/linux/99-realsense-libusb.rules /etc/udev/rules.d/99-realsense-libusb.rules
 
 sudo udevadm control --reload-rules
 sudo udevadm trigger
